06bcf8816c498355f522d060c4c2533ab8134936
giusant <giuliano.santiago@hotmail.com>
2016-04-25 07:49:46 -0500

    src
    Adicionado arquivos DAO

--- src/br/si/es/sga/dao/AlunoDAO.java
+++ src/br/si/es/sga/dao/AlunoDAO.java
@@ -1,15 +1,43 @@
 package br.si.es.sga.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.util.List;
+
+
 
 import br.si.es.sga.dto.AlunoDTO;
 import br.si.es.sga.exeception.PersistenciaException;
+import br.si.es.sga.jdbc.ConexaoUtil;
 
 public class AlunoDAO implements GenericDAO<AlunoDTO> {
 
 	@Override
 	public void inserir(AlunoDTO alunoDTO) throws PersistenciaException {
-		// TODO Auto-generated method stub
+		try{
+			//int chaveEnd = insereEndereco(alunoDTO.getIdEndereco());
+			Connection connection =  ConexaoUtil.getInstance().getConnection();
+			
+			String sql ="INSERT INTO TB_ALUNO(nomeAluno, dataNasc, dataVencimento, foto, dataMatricula, "
+					+ "telefoneAluno, modalidade_idModalidade, Endereco_idEndereco )" + " VALUES(?,?,?,?,?,?,?,?)";
+			PreparedStatement Statement = connection.prepareStatement(sql);
+			Statement.setString(1, alunoDTO.getNomeAluno());
+			Statement.setDate(2, alunoDTO.getDataNasc());
+			Statement.setDate(3,alunoDTO.getDataVencimento());
+			Statement.setObject(4, alunoDTO.getFoto());
+			Statement.setDate(5, alunoDTO.getDataMatricula());
+			Statement.setString(6, alunoDTO.getTelefoneAluno());
+			Statement.setInt(7, alunoDTO.getIdModalidade().getIdModalidade());
+			Statement.setInt(8, alunoDTO.getIdEndereco().getIdEndereco());
+			
+			Statement.execute();
+			connection.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			throw new PersistenciaException(e.getMessage(),e ) ;
+		}
+		
 		
 	}
 
---
+++ src/br/si/es/sga/dao/ModalidadeDAO.java
@@ -0,0 +1,40 @@
+package br.si.es.sga.dao;
+
+import java.util.List;
+
+import br.si.es.sga.dto.ModalidadeDTO;
+import br.si.es.sga.exeception.PersistenciaException;
+
+public class ModalidadeDAO implements GenericDAO<ModalidadeDTO>{
+
+	@Override
+	public void inserir(ModalidadeDTO obj) throws PersistenciaException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(ModalidadeDTO obj) throws PersistenciaException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(Integer id) throws PersistenciaException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ModalidadeDTO> listarTodos() throws PersistenciaException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModalidadeDTO buscarPorId(Integer id) throws PersistenciaException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
